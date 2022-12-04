649c54c9c6eb6afa936bdaf2e4b073e33a784b02
kerim-20 <74004776+kerim-20@users.noreply.github.com>
2022-12-04 14:41:04 +0100

    Add files via upload

---
+++ Blog/views/home/index.php
@@ -0,0 +1,5 @@
+<div class="text-center">
+	<h1>Welcome To Doctor's Board</h1>
+	<p class="lead">You want the help of an expert? You 're in the right place .</p>
+	<a class="btn btn-primary text-center" href="<?php echo ROOT_PATH;?>shares">Share Now</a>
+</div>
